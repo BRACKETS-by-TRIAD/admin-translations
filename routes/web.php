d65bb29ffc45185ad60d42a143ab2599b7626cd9
Pavol Perdík <palypster@gmail.com>
2017-08-17 16:11:27 +0200

    fixing routes middleware

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => 'bindings'], function(){
+Route::group(['middleware' => 'web'], function(){
     Route::get('/admin/translation','\Brackets\AdminTranslations\Http\Controllers\Admin\TranslationsController@index');
     Route::post('/admin/translation/{translation}','\Brackets\AdminTranslations\Http\Controllers\Admin\TranslationsController@update');
 });
