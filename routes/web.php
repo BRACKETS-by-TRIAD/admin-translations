7b30c09130d82b881f4fc5ea0126214ef4b19e2d
Pavol Perdík <palypster@gmail.com>
2017-08-23 17:14:13 +0200

    Removed admin middleware - because this makes this package dependent on admin-auth. Do we want id?

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web', 'admin']], function(){
+Route::group(['middleware' => ['web']], function(){
     Route::get('/admin/translation','\Brackets\AdminTranslations\Http\Controllers\Admin\TranslationsController@index');
     Route::post('/admin/translation/{translation}','\Brackets\AdminTranslations\Http\Controllers\Admin\TranslationsController@update');
 });
