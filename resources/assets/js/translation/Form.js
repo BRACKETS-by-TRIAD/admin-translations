cbc3bafeaa1918b9fd19c1f008eaa0586b103718
Pavol Perdík <palypster@gmail.com>
2017-08-25 10:34:58 +0200

    moving assets into package

---
+++ resources/assets/js/translation/Form.js
@@ -0,0 +1,5 @@
+import BaseForm from 'components/Form/BaseForm';
+
+Vue.component('translation-form', {
+    mixins: [BaseForm]
+});
---
+++ resources/assets/js/translation/Listing.js
@@ -0,0 +1,73 @@
+import BaseListing from 'components/Listing/BaseListing';
+
+Vue.component('translation-listing', {
+
+    mixins: [BaseListing],
+
+    props: {
+        label: {
+            type: String,
+            default: function() {
+                return 'All groups';
+            }
+        }
+    },
+
+    data(){
+        return {
+
+            filters: {
+                group: null
+            },
+
+            translationId: null,
+            translationDefault: '',
+            translations: {}
+        }
+    },
+
+    computed: {
+        filteredGroup() {
+            return this.filters.group === null ? this.label : this.filters.group;
+        },
+    },
+
+    methods: {
+
+        filterGroup(group) {
+            this.filters.group = group;
+            this.loadData(true);
+        },
+
+        resetGroup() {
+            this.filters.group = null;
+            this.loadData(true);
+        },
+
+        editTranslation (item) {
+            this.$modal.show('edit-translation', item);
+        },
+        beforeModalOpen ({params}) {
+            this.translationId = params.id;
+            this.translationDefault = params.key;
+            this.translations = {};
+            for (const key of Object.keys(params.text)) {
+                this.translations[key] = params.text[key];
+            }
+        },
+        onSubmit() {
+            let url = '/admin/translation/'+this.translationId;
+            let data = {
+                text: this.translations
+            };
+
+            axios.post(url, data).then(response => {
+                this.$modal.hide('edit-translation');
+                this.$notify({ type: 'success', title: 'Success!', text: 'Item successfully changed.'});
+                this.loadData();
+            }, error => {
+                this.$notify({ type: 'error', title: 'Error!', text: 'An error has occured.'});
+            });
+        }
+    }
+});
