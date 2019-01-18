6d8c8e06c0e5eb8b580c4f920d16ba5982d69a47
David Běhal <behal.david@gmail.com>
2019-01-18 14:12:40 +0100

    Removed middlewares

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-Route::group(['middleware' => ['web', 'auth:' . config('admin-auth.defaults.guard'), 'admin']], function(){
+Route::group(['middleware' => ['web']], function(){
     Route::namespace('Brackets\AdminTranslations\Http\Controllers\Admin')->group(function () {
         Route::get('/admin/translations',                   'TranslationsController@index');
         Route::post('/admin/translations/rescan',           'RescanTranslationsController@rescan');
